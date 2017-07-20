a5169db09df50d64bd64a6c639991496b5b34252
chenyu <yuchen004063@sohu-inc.com>
2017-07-20 18:22:17 +0800

    commit homeppage again

---
+++ src/components/slider.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="slider">
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide"></div>
+          <div class="swiper-slide"></div>
+          <div class="swiper-slide"></div>
+          <div class="swiper-slide"></div>
+          <div class="swiper-slide"></div>
+        </div>
+      </div>
+    </div>
+</template>
+<script>
+  export default {
+
+  }
+</script>
+<style>
+
+</style>
--- src/pages/homePage.vue
+++ src/pages/homePage.vue
@@ -1,37 +1,12 @@
 <template>
-  <div id="foo" class="main-list">
-    <div class="list-header">
-      <ul class="ul-list">
-        <li><img src="../assets/images/Category.png" alt="icon"></li>
-        <li class="home"><span>首页</span></li>
-        <li><img src="../assets/images/remind.png" alt="icon"></li>
-        <li><img src="../assets/images/browse.png" alt="icon"></li>
-      </ul>
-    </div>
-  </div>
+
 </template>
 <script>
+
   export default {
     name: 'homePage'
   }
 </script>
 <style lang="scss">
-  .main-list{
-    position: fixed;
-    -webkit-transform: translateZ(0);
-    transform: translateZ(0);
-    top: 0;
-    z-index: 4;
-    height: 12%;
-    width: 100%;
-    background: #00a2ea;
-  }
-  .ul-list li{
-    display: inline-block;
-    margin:1%;
-  }
-  .ul-list .home span{
-    margin-right:16rem;
-    color:#fff;
-  }
+
 </style>
